a6df1b5ce39c118c67739834d3c4252e2eb0e05d
ploddasha <ploddasha@yandex.ru>
2022-09-26 23:06:25 +0700

    -_-

--- Task-1-1-1/src/main/java/org/example/HeapSort.java
+++ Task-1-1-1/src/main/java/org/example/HeapSort.java
@@ -1,25 +1,25 @@
 package org.example;
 
 public class HeapSort {
-    public static void hsort(int arr[], int n)
-    {
-        for (int i=n/2-1; i>=0; i--)
-            heapify(arr,n,i);
-        for (int i=n-1; i>0; i--)
+    public static void hsort(int arr[], int n) {
+        for (int i = (n / 2 - 1); i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = (n - 1); i > 0; i--)
         {
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
-            heapify(arr,i, 0);
+            heapify(arr, i, 0);
         }
     }
     public static void heapify(int arr[], int n, int i)
     {
         int maxid = i;
 
-        if ((2*i+2)<n && arr[maxid] < arr[2 * i + 2])
+        if ((2*i+2) < n && arr[maxid] < arr[2 * i + 2])
             maxid = 2 * i + 2;
-        if ((2*i+1)<n && arr[maxid] < arr[2 * i + 1])
+        if ((2*i+1) < n && arr[maxid] < arr[2 * i + 1])
             maxid = 2 * i + 1;
 
         if (maxid != i) {
--- Task-1-1-1/src/test/java/org/example/HeapSortTest.java
+++ Task-1-1-1/src/test/java/org/example/HeapSortTest.java
@@ -1,54 +1,54 @@
 package org.example;
 
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
 
 class HeapSortTest {
 
     private HeapSort heapSort = new HeapSort();
 
     @Test
-    void test1() {
+    void testRegularArray() {
         final int[] testArray = {6, 3, 7, 1, 9, 5};
         final int[] expectedArray = {1, 3, 5, 6, 7, 9};
-        heapSort.hsort(testArray,6);
+        heapSort.hsort(testArray, 6);
 
         assertArrayEquals(testArray, expectedArray);
     }
 
     @Test
-    void test2() {
+    void testOneElementArray() {
         final int[] testArray = {3};
         final int[] expectedArray = {3};
-        heapSort.hsort(testArray,1);
+        heapSort.hsort(testArray, 1);
 
         assertArrayEquals(testArray, expectedArray);
     }
 
     @Test
-    void test3() {
+    void testEmptyArray() {
         final int[] testArray = {};
         final int[] expectedArray = {};
-        heapSort.hsort(testArray,0);
+        heapSort.hsort(testArray, 0);
 
         assertArrayEquals(testArray, expectedArray);
     }
 
     @Test
-    void test4() {
+    void testReversedArray() {
         final int[] testArray = {11, 9, 7, 7, 4};
-        final int[] expectedArray = {4,7,7,9,11};
-        heapSort.hsort(testArray,5);
+        final int[] expectedArray = {4, 7, 7, 9, 11};
+        heapSort.hsort(testArray, 5);
 
         assertArrayEquals(testArray, expectedArray);
     }
 
     @Test
-    void test5() {
+    void testNegativeArray() {
         final int[] testArray = {6, 3, -7, 1, 9, 5};
         final int[] expectedArray = {-7, 1, 3, 5, 6, 9};
-        heapSort.hsort(testArray,6);
+        heapSort.hsort(testArray, 6);
 
         assertArrayEquals(testArray, expectedArray);
     }
