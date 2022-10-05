639d61b65d093f811b94edb1a35d164cdc53388f
ploddasha <ploddasha@users.noreply.github.com>
2022-10-05 14:15:41 +0000

    Deploying to gh-pages from @ ploddasha/Plodushcheva_OOP@3efb2e42fc8e2abaf276cc9a9dadc872a4c069f1 🚀

--- Task-1-2-1/type-search-index.js
+++ Task-1-2-1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Stack"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Stack"},{"p":"ru.nsu.plodushcheva","l":"Stack"}]
