bcd48642504672dec9216176a27c7b97e942ea49
ploddasha <ploddasha@yandex.ru>
2022-11-21 00:38:19 +0700

    reviewdog

--- Task-1-3-1/src/main/java/ru/nsu/plodushcheva/SubString.java
+++ Task-1-3-1/src/main/java/ru/nsu/plodushcheva/SubString.java
@@ -1,20 +1,37 @@
 package ru.nsu.plodushcheva;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 
+/**
+ * SubString finds the specified substring in the text.
+ * The text is read in a stream, using the Knutt-Maurice-Pratt algorithm
+ */
 public class SubString {
 
-    public static ArrayList<Integer> subStringFinder (String sub,File fileName) throws IOException {
+    /**
+     * @param sub is the string whose occurrences we are looking for
+     * @param fileName the name of the file with text
+     * @return array with indices of substring occurrences
+     * @throws IOException
+     */
+    public static ArrayList<Integer> subStringFinder (String sub, File fileName) throws IOException {
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8))) {
+        FileInputStream file = new FileInputStream(fileName);
+        try (BufferedReader reader = new BufferedReader (new InputStreamReader (file, StandardCharsets.UTF_8)))
+        {
             ArrayList<Integer> result = new ArrayList<>();
-            kmp(reader,sub, result);
+            kmp(reader, sub, result);
             return result;
         }
-        catch (IOException e) {
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
 
@@ -22,11 +39,17 @@ public class SubString {
     }
 
 
+    /**
+     * @param text is source text
+     * @param sub is the string whose occurrences we are looking for
+     * @param arr is array with indices of substring occurrences
+     * @throws IOException
+     */
     static void kmp (BufferedReader text, String sub, ArrayList<Integer> arr) throws IOException {
 
-        int M = sub.length();
-        int lps[] = new int[M];
-        computeLPSArray(sub, lps);
+        int len = sub.length();
+        int[] lps = new int[len];
+        computePrefixArray(sub, lps);
 
         int i = 0;
         int j = 0;
@@ -37,14 +60,14 @@ public class SubString {
                 j++;
                 i++;
             }
-            if (j == M) {
-                arr.add(i-j);
+            if (j == len) {
+                arr.add(i - j);
                 j = lps[j - 1];
             }
-            else if ( a!=-1 && sub.charAt(j) != (char) a) {
+            else if (a != -1 && sub.charAt(j) != (char) a) {
                 if (j != 0) {
                     j = lps[j - 1];
-                }else {
+                } else {
                     a = text.read();
                     i = i + 1;
                 }
@@ -52,15 +75,22 @@ public class SubString {
         }
     }
 
-    static void computeLPSArray(String sub, int lps[])
+    /**
+     * @param sub is the string whose occurrences we are looking for
+     * @param lps a prefix function,
+     * i.e. an array of numbers is defined as follows:
+     * element is the longest length of the largest self suffix of a substring
+     * that matches its prefix (self suffix means not matching the whole string).
+     */
+    static void computePrefixArray(String sub, int[] lps)
     {
-        int M = sub.length();
+        int len = sub.length();
         lps[0] = 0;
 
-        for (int i=1; i<M; ++i) {
-            int j = lps[i-1];
+        for (int i = 1; i < len; ++i) {
+            int j = lps[i - 1];
             while (j > 0 && sub.charAt(i) != sub.charAt(j))
-                j = lps[j-1];
+                j = lps[j - 1];
             if (sub.charAt(i) == sub.charAt(j)){
                 ++j;
             }
