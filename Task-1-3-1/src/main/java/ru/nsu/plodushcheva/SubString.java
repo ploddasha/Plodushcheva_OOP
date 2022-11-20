90acb35dc2dedf4f3f676a4d488ce2ff536d6b32
ploddasha <ploddasha@yandex.ru>
2022-11-21 01:36:33 +0700

    reviewdog again

--- Task-1-3-1/src/main/java/ru/nsu/plodushcheva/SubString.java
+++ Task-1-3-1/src/main/java/ru/nsu/plodushcheva/SubString.java
@@ -2,8 +2,8 @@ package ru.nsu.plodushcheva;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -18,12 +18,13 @@ public class SubString {
     /**
      * file stream is passed to the InputStreamReader object,
      * which converts the byte stream to character stream.
+     *
      * @param sub is the string whose occurrences we are looking for
      * @param fileName the name of the file with text
      * @return array with indices of substring occurrences
-     * @throws IOException
+     * @throws IOException while reading file
      */
-    public static ArrayList<Integer> subStringFinder (String sub, File fileName)
+    public static ArrayList<Integer> subStringFinder(String sub, File fileName)
             throws IOException {
 
         FileInputStream file = new FileInputStream(fileName);
@@ -46,12 +47,13 @@ public class SubString {
      * that when a mismatch occurs, the word itself embodies sufficient information to determine
      * where the next match could begin,
      * thus bypassing re-examination of previously matched characters.
+     *
      * @param text is source text
      * @param sub is the string whose occurrences we are looking for
      * @param arr is array with indices of substring occurrences
-     * @throws IOException
+     * @throws IOException wile working with text
      */
-    static void kmp (BufferedReader text, String sub, ArrayList<Integer> arr) throws IOException {
+    static void kmp(BufferedReader text, String sub, ArrayList<Integer> arr) throws IOException {
 
         int len = sub.length();
         int[] lps = new int[len];
@@ -69,8 +71,7 @@ public class SubString {
             if (j == len) {
                 arr.add(i - j);
                 j = lps[j - 1];
-            }
-            else if (a != -1 && sub.charAt(j) != (char) a) {
+            } else if (a != -1 && sub.charAt(j) != (char) a) {
                 if (j != 0) {
                     j = lps[j - 1];
                 } else {
@@ -86,11 +87,11 @@ public class SubString {
      * i.e. an array of numbers is defined as follows:
      * element is the longest length of the largest self suffix of a substring
      * that matches its prefix (self suffix means not matching the whole string).
+     *
      * @param sub is the string whose occurrences we are looking for
      * @param lps a prefix array
      */
-    static void computePrefixArray(String sub, int[] lps)
-    {
+    static void computePrefixArray(String sub, int[] lps) {
         int len = sub.length();
         lps[0] = 0;
 
