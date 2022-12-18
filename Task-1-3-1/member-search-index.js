7b90becf00e9e59e1963d58a26d7368a93a37bbe
ploddasha <ploddasha@users.noreply.github.com>
2022-12-18 11:17:20 +0000

    Deploying to gh-pages from @ ploddasha/Plodushcheva_OOP@f2a9d1267a8899b5efd00a55442292e00d567590 🚀

--- Task-1-3-1/member-search-index.js
+++ Task-1-3-1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"ru.nsu.plodushcheva","c":"SubString","l":"SubString()","url":"%3Cinit%3E()"},{"p":"ru.nsu.plodushcheva","c":"SubString","l":"subStringFinder(String, File)","url":"subStringFinder(java.lang.String,java.io.File)"}]
+memberSearchIndex = [{"p":"ru.nsu.plodushcheva","c":"SubString","l":"SubString()","url":"%3Cinit%3E()"},{"p":"ru.nsu.plodushcheva","c":"SubString","l":"subStringFinder(String, InputStream)","url":"subStringFinder(java.lang.String,java.io.InputStream)"}]
