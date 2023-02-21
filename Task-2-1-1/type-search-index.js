69eb9134f284f65e8b45f37b77e2758ff29615cc
ploddasha <ploddasha@users.noreply.github.com>
2023-02-21 05:33:10 +0000

    Deploying to gh-pages from @ ploddasha/Plodushcheva_OOP@bf2d221d4a67600920adab01fd811bde1d713c85 🚀

---
+++ Task-2-1-1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"nsu.ru.plodushcheva","l":"MultiThread.DifferentThread"},{"p":"nsu.ru.plodushcheva","l":"MultiThread"},{"p":"nsu.ru.plodushcheva","l":"ParallelsStream"},{"p":"nsu.ru.plodushcheva","l":"Prime"},{"p":"nsu.ru.plodushcheva","l":"Successively"}]
